0a2a545331507fb07900dd5da7548e1dfc41bcb9
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-10-02 13:27:05 +0900

    Add ELPA_PATH environment variable in test

--- targets/melpa-init.el
+++ targets/melpa-init.el
@@ -5,4 +5,10 @@
       package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 
+(let ((env (getenv "ELPA_PATH")))
+  (when env
+    (setq package-directory-list
+          (append (split-string env ":")
+                  package-directory-list))))
+
 (package-initialize)
